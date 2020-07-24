577971650b4016230fa352f3437d20856ec11872
umerk4466 <umerk4466@gmail.com>
2020-07-24 11:48:49 +0100

    updated

---
+++ components/CustomModel.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import globalStyle from "../constants/styles";
+import Colors from "../constants/colors";
+
+// CustomModel Component
+const CustomModel = props => {
+  return <View></View>;
+};
+
+export default CustomModel;
