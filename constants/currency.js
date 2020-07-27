e22106ec9bc1cfc75e229cc0c138570e530241b5
umerk4466 <umerk4466@gmail.com>
2020-07-27 22:44:15 +0100

    Updated

---
+++ constants/currency.js
@@ -0,0 +1,17 @@
+// // all currencies list
+// export const CurrencySigns = {
+//   pound: "£",
+//   dollar: "$",
+//   euro: "€",
+// };
+
+// // application selected currency sign
+// export const AppCurrency = {
+//   sign: CurrencySigns.pound,
+// };
+
+// const AppCurrency = (Currencies) => {
+//     return sign: Currencies,
+//   };
+
+//   export default SetHeaderMessage;
