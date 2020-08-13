2b1252be6757d6a1a4e08840ebb104fcf235bf1a
umerk4466 <umerk4466@gmail.com>
2020-08-13 16:16:35 +0100

    Updated

--- screens/MortgageCalcScreen.js
+++ screens/MortgageCalcScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { InteractionManager } from "react-native";
 // import components
 import RootComponent from "../components/RootComponent";
+import CustomLoader from "../components/CustomLoader";
 import HeadingText from "../components/HeadingText";
 import BoxWrapper from "../components/BoxWrapper";
 import CustomMoneyInput from "../components/CustomMoneyInput";
@@ -29,86 +30,97 @@ const ValidatorSchema = yup.object({
 const MortgageCalcScreen = ({ navigation }) => {
   // imported function to add right button on the header
   SetHeaderMessage(navigation, ScreenMessage);
-  return (
-    <Formik
-      initialValues={{
-        property_price: "",
-        available_deposit: "",
-        interest_rate_percentage: 2.7,
-        mortgage_term_years: 25,
-      }}
-      validationSchema={ValidatorSchema}
-      enableReinitialize={true}
-      onSubmit={(values, actions) => {
-        // calculation function
-        MortgageCalcScreenFunction({ values, navigation });
-      }}
-    >
-      {(props) => (
-        <RootComponent>
-          {/* Property details heading and container */}
-          <HeadingText paddingTopNone heading="Property and mortgage details" />
-          <BoxWrapper>
-            {/*Price of the property field */}
-            <CustomMoneyInput
-              title={"Price of the property"}
-              placeholder={"£125,000"}
-              onBlur={props.handleBlur("property_price")}
-              value={props.values.property_price}
-              onChangeText={(maskedText, rawText) => {
-                props.setFieldValue("property_price", rawText);
-              }}
-              error={props.errors.property_price}
-              touched={props.touched.property_price}
+  // state for loader gif
+  const [isLoading, setIsLoading] = React.useState(true);
+  InteractionManager.runAfterInteractions(() => setIsLoading(false));
+  // return component
+  if (isLoading) {
+    return <CustomLoader />;
+  } else {
+    return (
+      <Formik
+        initialValues={{
+          property_price: "",
+          available_deposit: "",
+          interest_rate_percentage: 2.7,
+          mortgage_term_years: 25,
+        }}
+        validationSchema={ValidatorSchema}
+        enableReinitialize={true}
+        onSubmit={(values, actions) => {
+          // calculation function
+          MortgageCalcScreenFunction({ values, navigation });
+        }}
+      >
+        {(props) => (
+          <RootComponent>
+            {/* Property details heading and container */}
+            <HeadingText
+              paddingTopNone
+              heading="Property and mortgage details"
             />
-            {/* Available deposit field */}
-            <CustomMoneyInput
-              title={"Available deposit"}
-              placeholder={"£30,000"}
-              onBlur={props.handleBlur("available_deposit")}
-              value={props.values.available_deposit}
-              onChangeText={(maskedText, rawText) => {
-                props.setFieldValue("available_deposit", rawText);
-              }}
-              error={props.errors.available_deposit}
-              touched={props.touched.available_deposit}
-            />
-            {/* Interest rate (%) slider */}
-            <CustomSlider
-              title={"Interest rate"}
-              start={0}
-              end={25}
-              floatValue
-              value={props.values.interest_rate_percentage}
-              onValueChange={(value) => {
-                props.setFieldValue("interest_rate_percentage", value);
-              }}
-              step={0.1}
-              suffix={"%"}
-            />
-            {/* Mortgage term (years) slider */}
-            <CustomSlider
-              title={"Mortgage term"}
-              start={5}
-              end={40}
-              value={props.values.mortgage_term_years}
-              onValueChange={(value) => {
-                props.setFieldValue("mortgage_term_years", value);
-              }}
-              step={5}
-              suffix={"years"}
-            />
-          </BoxWrapper>
-          {/* Calculate and reset button */}
-          <CalculateResetButton
-            onPressCalculateBtn={props.handleSubmit}
-            calculateBtnTittle="Calculate Payments"
-            onPressResetBtn={props.resetForm}
-          ></CalculateResetButton>
-        </RootComponent>
-      )}
-    </Formik>
-  );
+            <BoxWrapper>
+              {/*Price of the property field */}
+              <CustomMoneyInput
+                title={"Price of the property"}
+                placeholder={"£125,000"}
+                onBlur={props.handleBlur("property_price")}
+                value={props.values.property_price}
+                onChangeText={(maskedText, rawText) => {
+                  props.setFieldValue("property_price", rawText);
+                }}
+                error={props.errors.property_price}
+                touched={props.touched.property_price}
+              />
+              {/* Available deposit field */}
+              <CustomMoneyInput
+                title={"Available deposit"}
+                placeholder={"£30,000"}
+                onBlur={props.handleBlur("available_deposit")}
+                value={props.values.available_deposit}
+                onChangeText={(maskedText, rawText) => {
+                  props.setFieldValue("available_deposit", rawText);
+                }}
+                error={props.errors.available_deposit}
+                touched={props.touched.available_deposit}
+              />
+              {/* Interest rate (%) slider */}
+              <CustomSlider
+                title={"Interest rate"}
+                start={0}
+                end={25}
+                floatValue
+                value={props.values.interest_rate_percentage}
+                onValueChange={(value) => {
+                  props.setFieldValue("interest_rate_percentage", value);
+                }}
+                step={0.1}
+                suffix={"%"}
+              />
+              {/* Mortgage term (years) slider */}
+              <CustomSlider
+                title={"Mortgage term"}
+                start={5}
+                end={40}
+                value={props.values.mortgage_term_years}
+                onValueChange={(value) => {
+                  props.setFieldValue("mortgage_term_years", value);
+                }}
+                step={5}
+                suffix={"years"}
+              />
+            </BoxWrapper>
+            {/* Calculate and reset button */}
+            <CalculateResetButton
+              onPressCalculateBtn={props.handleSubmit}
+              calculateBtnTittle="Calculate Payments"
+              onPressResetBtn={props.resetForm}
+            ></CalculateResetButton>
+          </RootComponent>
+        )}
+      </Formik>
+    );
+  }
 };
 
 export default MortgageCalcScreen;
