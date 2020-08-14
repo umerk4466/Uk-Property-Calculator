27b9bb799eeced321f1b880546b202f80ce4220a
umerk4466 <umerk4466@gmail.com>
2020-08-14 15:06:31 +0100

    Updated

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,60 +1,70 @@
 import React from "react";
+import { InteractionManager } from "react-native";
 // import components
 import RootComponent from "../components/RootComponent";
+import CustomLoader from "../components/CustomLoader";
 import TouchableButton from "../components/TouchableButton";
 
 export default function HomeScreen({ navigation }) {
-  return (
-    <RootComponent noPadding>
-      {/* ROI CALCULATOR */}
-      <TouchableButton
-        onPress={() => navigation.navigate("ROI Calculator")}
-        imgSource={require("../assets/HomeScreen/roi.png")}
-        title="ROI Calculator"
-        description="Return on investment"
-      ></TouchableButton>
-      {/* BTL CALCULATOR */}
-      <TouchableButton
-        onPress={() => navigation.navigate("BTL Calculator")}
-        imgSource={require("../assets/HomeScreen/pound.png")}
-        title="BTL Calculator"
-        description="Buy To Let Calculations"
-      ></TouchableButton>
-      {/* Net Rental Yield Calculator */}
-      {/* <TouchableButton
+  // state for loader gif
+  const [isLoading, setIsLoading] = React.useState(true);
+  InteractionManager.runAfterInteractions(() => setIsLoading(false));
+  // return component
+  if (isLoading) {
+    return <CustomLoader />;
+  } else {
+    return (
+      <RootComponent noPadding>
+        {/* ROI CALCULATOR */}
+        <TouchableButton
+          onPress={() => navigation.navigate("ROI Calculator")}
+          imgSource={require("../assets/HomeScreen/roi.png")}
+          title="ROI Calculator"
+          description="Return on investment"
+        ></TouchableButton>
+        {/* BTL CALCULATOR */}
+        <TouchableButton
+          onPress={() => navigation.navigate("BTL Calculator")}
+          imgSource={require("../assets/HomeScreen/pound.png")}
+          title="BTL Calculator"
+          description="Buy To Let Calculations"
+        ></TouchableButton>
+        {/* Net Rental Yield Calculator */}
+        {/* <TouchableButton
           onPress={() => navigation.navigate("Net Rental Yield")}
           imgSource={require("../assets/HomeScreen/net_rental_yield.png")}
           title="Net Rental Yield"
           description="Investment Property Calculator"
         ></TouchableButton> */}
-      {/* MORTGAGE CALCULATOR */}
-      <TouchableButton
-        onPress={() => navigation.navigate("Mortgage Calculator")}
-        imgSource={require("../assets/HomeScreen/mortgage.png")}
-        title="Mortgage Calculator"
-        description="Find monthly mortgage payments"
-      ></TouchableButton>
-      {/* AREA CALCULATOR */}
-      {/* <TouchableButton
+        {/* MORTGAGE CALCULATOR */}
+        <TouchableButton
+          onPress={() => navigation.navigate("Mortgage Calculator")}
+          imgSource={require("../assets/HomeScreen/mortgage.png")}
+          title="Mortgage Calculator"
+          description="Find monthly mortgage payments"
+        ></TouchableButton>
+        {/* AREA CALCULATOR */}
+        {/* <TouchableButton
           onPress={() => navigation.navigate("Price Per Area")}
           imgSource={require("../assets/HomeScreen/area.png")}
           title="Area Calculator"
           description="Price to Sqmetre/Sqfoot"
         ></TouchableButton> */}
-      {/* LTV CALCULATOR */}
-      <TouchableButton
-        onPress={() => navigation.navigate("LTV Calculator")}
-        imgSource={require("../assets/HomeScreen/affordability.png")}
-        title="LTV Calculator"
-        description="Loan to Value you'll have to borrow"
-      ></TouchableButton>
-      {/* Borrowing CALCULATOR */}
-      <TouchableButton
-        onPress={() => navigation.navigate("Borrowing Calculator")}
-        imgSource={require("../assets/HomeScreen/mortgage.png")}
-        title="Borrowing Calculator"
-        description="How much mortgage I can get"
-      ></TouchableButton>
-    </RootComponent>
-  );
+        {/* LTV CALCULATOR */}
+        <TouchableButton
+          onPress={() => navigation.navigate("LTV Calculator")}
+          imgSource={require("../assets/HomeScreen/affordability.png")}
+          title="LTV Calculator"
+          description="Loan to Value you'll have to borrow"
+        ></TouchableButton>
+        {/* Borrowing CALCULATOR */}
+        <TouchableButton
+          onPress={() => navigation.navigate("Borrowing Calculator")}
+          imgSource={require("../assets/HomeScreen/mortgage.png")}
+          title="Borrowing Calculator"
+          description="How much mortgage I can get"
+        ></TouchableButton>
+      </RootComponent>
+    );
+  }
 }
