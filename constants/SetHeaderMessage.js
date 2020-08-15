72521d08a4aadb7b0b667ce55c6eca60d5900423
umerk4466 <umerk4466@gmail.com>
2020-08-15 15:46:27 +0100

    Updated

--- constants/SetHeaderMessage.js
+++ constants/SetHeaderMessage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { Alert } from "react-native";
 import { Icon } from "react-native-elements";
 import Colors from "../constants/colors";
@@ -8,12 +9,13 @@ const SetHeaderMessage = (navigation, message) => {
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Icon
-          name="info"
-          iconStyle={{ paddingRight: 12 }}
-          color={Colors.OnButtonColor}
-          onPress={() => Alert.alert("Info", message)}
-        />
+        <TouchableOpacity onPress={() => Alert.alert("Info", message)}>
+          <Icon
+            name="info"
+            iconStyle={{ padding: 12 }}
+            color={Colors.OnButtonColor}
+          />
+        </TouchableOpacity>
       ),
     });
   }, [navigation]);
